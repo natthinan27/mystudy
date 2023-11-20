e643f3c6e22655c435a47e99bb8c4da9530cbdd1
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-11-21 03:43:49 +0700

    .

--- pages/AboutMe.py
+++ pages/AboutMe.py
@@ -4,9 +4,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
-st.header("")
 
-st.image(".pages/img/s1.jpg")
+st.header("")
+img {
+            border-radius: 2em;
+        }
+        """,
+    ):
+        st.image("./s1.jpg")
+
+#st.image(".pages/img/s1.jpg")
 
 html_8 = """
 <div style="background-color:#6BD5DA;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
