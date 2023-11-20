775175f92627274033e6be97207be55a0043783a
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-11-21 02:26:13 +0700

    .

--- pages/Home.py
+++ pages/Home.py
@@ -7,9 +7,17 @@ st.set_page_config(
     page_title="การวิเคราะห์พฤติกรรมผู้บริโภคโดยการจัดกลุ่มแบบเคมีน",
     page_icon= ":bar_chart:",
 )
-
 html_1 = """
-<div style="background-color:#dab3ff;padding:10px;border-radius:10px 10px 10px 10px;border-style:'solid';border-color:black">
+<div style="background-color:#0E1117;padding:5px;border-radius:5px;border-bottom: 5px solid #ffffff;border-top: 5px solid #ffffff;">
+<center><h4>การวิเคราะห์พฤติกรรมผู้บริโภคโดยการจัดกลุ่มแบบเคมีน</h4><h5>Analyzes Consumer Behavior by K-means clustering</h5></center>
+</div>
+"""
+st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")
+
+
+html_ = """
+<div style="background-color:#433A4A;padding:10px;border-radius:10px 10px 10px 10px;border-style:'solid';border-color:black">
 <center><h5>การวิเคราะห์พฤติกรรมผู้บริโภคโดยการจัดกลุ่มแบบเคมีน</h5></center>
 <center><h5>Analyzes Consumer Behavior by K-means clustering</h5></center>
 </div>
