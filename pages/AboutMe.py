bb8b5441959f5a56f9f75dbb65c0ac5f6953a85d
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-11-21 03:35:57 +0700

    .

---
+++ pages/AboutMe.py
@@ -0,0 +1,15 @@
+
+st.header("")
+
+st.image("./img/s1.jpg")
+
+
+html_8 = """
+<div style="background-color:#6BD5DA;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>นางสาวณัฐินันท์ เข็มทอง 644285002 64/44</h5></center>
+</div>
+"""
+
+st.markdown(html_8, unsafe_allow_html=True)
+st.markdown("")
+
