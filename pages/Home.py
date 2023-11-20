ee01c0065f4db3860b68e6cd32c49a50811212c6
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-11-21 02:53:50 +0700

    .

--- pages/Home.py
+++ pages/Home.py
@@ -15,8 +15,14 @@ html_1 = """
 st.markdown(html_1, unsafe_allow_html=True)
 st.markdown("")
 
-
 html_2 = """
+<center><h5>บทคัดย่อ</h5></center>
+</div>
+"""
+st.markdown(html_2, unsafe_allow_html=True)
+st.markdown("")
+
+html_3 = """
 <div style="background-color:#32174d;padding:10px;border-radius:5px ;border 5px solid #ffffff;">
 <center><h5>วัตถุประสงค์ของงานวิจัยนี้ คือ เพื่อช่วยธุรกิจหรือองค์กรในการทำให้เข้าใจลึกซึ้งเกี่ยวกับลูกค้าและตลาดของพวกเขา เพื่อให้สามารถดำเนินกิจการได้อย่างมีประสิทธิภาพมากยิ่งขึ้น นอกจากนี้การวิเคราะห์พฤติกรรมผู้บริโภคช่วยในการทำความเข้าใจถึงความต้องการของลูกค้าและที่สำคัญที่สุดในการพัฒนาผลิตภัณฑ์หรือบริการที่ตอบสนองต่อความต้องการนี้ ดังนั้นการวิเคราะห์พฤติกรรมผู้บริโภคเป็นเครื่องมือที่มีประโยชน์ในการทำให้ธุรกิจทราบถึงทิศทางที่เหมาะสมที่สุดในการพัฒนาและเติบโตขึ้นไปในอนาคต จึงได้ทำการทดลองการจัดกลุ่มแบบเคมีน กับชุดข้อมูลConsumer Behavior and Shopping Habits Datasetข้อมูลประกอบด้วย 1,000 แถว18 คอลัมน์ 
 ผลการทดลอง วิธีการหาค่า Silhouette Score และ หาค่า Calinski-Harabasz Score ให้ประสิทธิภาพสูงสุดจะได้ค่าจะเห็นได้ว่า ค่า Silhouette Score ที่ได้คือ0.5368227965812387และ ค่า Calinski-Harabasz Score ที่ได้คือ 503.04653062320193
@@ -25,6 +31,6 @@ html_2 = """
 </div>
 </div>
 """
-st.markdown(html_2, unsafe_allow_html=True)
+st.markdown(html_3, unsafe_allow_html=True)
 st.markdown("")
 
