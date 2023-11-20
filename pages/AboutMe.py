47a236e0b6eed8dc5d6bdc080a9f31d8eee8d543
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-11-21 03:36:59 +0700

    .

--- pages/AboutMe.py
+++ pages/AboutMe.py
@@ -1,4 +1,9 @@
-
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
 st.header("")
 
 st.image("./img/s1.jpg")
