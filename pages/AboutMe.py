c99a448eda18752d57a3219c580f5e7be71e3ec4
natthinan27 <119989591+natthinan27@users.noreply.github.com>
2023-11-21 13:26:42 +0700

    .

--- pages/AboutMe.py
+++ pages/AboutMe.py
@@ -10,7 +10,7 @@ st.header("")
 st.image("./img/s1.jpg")
 
 html_8 = """
-<div style="background-color:#6BD5DA;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<div style="background-color:#6e91df;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h5>นางสาวณัฐินันท์ เข็มทอง 644285002 64/44</h5></center>
 </div>
 """
